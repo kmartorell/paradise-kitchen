81137f392f92fb3436703f0a3336fab5482ad6b7
EvanVerdu <89754847+EvanVerdu@users.noreply.github.com>
2023-03-23 21:30:06 -0400

    Update Register.js

--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -8,6 +8,60 @@ import { register } from './loginAnimation';
 
 function Register()
 {
+    console.log("In Register");
+
+    const newUser = { /* Gathers User input to Register */
+        RFirstName: document.getElementById("registerNameFirst").value,  
+        RLastName: document.getElementById("registerNameLast").value,
+        REmail: document.getElementById("registerEmail").value,
+        RUsername: document.getElementById("registerUsername").value,
+        RPassword: document.getElementById("registerPassword").value,
+    }
+
+    if(newUser.RFirstName == ''){
+        document.getElementById('registerNameFirst').placeholder = "Please enter a first name!";
+        document.getElementById('registerNameFirst').style.borderColor = "red";
+    }else {
+        document.getElementById('registerNameFirst').style.borderColor = "green";
+    }
+
+    if(newUser.RLastName == ''){
+        document.getElementById("registerNameLast").placeholder='Please enter a last name!';
+        document.getElementById('registerNameLast').style.borderColor = "red";
+    }else {
+        document.getElementById('registerNameLast').style.borderColor = "green";
+    }
+    
+    if(newUser.REmail == ''){
+        document.getElementById("registerEmail").placeholder='Please enter an email!';
+        document.getElementById('registerEmail').style.borderColor = "red";
+    }else {
+        document.getElementById('registerEmail').style.borderColor = "green";
+    }
+
+    if(newUser.RUsername == ''){
+        document.getElementById("registerUsername").placeholder='Please enter a username!';
+        document.getElementById('registerUsername').style.borderColor = "red";
+    }else {
+        document.getElementById('registerUsername').style.borderColor = "green";
+    }
+
+    if(newUser.RPassword == ''){
+        document.getElementById("registerPassword").placeholder='Please enter a password!';
+        document.getElementById('registerPassword').style.borderColor = "red";
+    }else {
+        document.getElementById('registerPassword').style.borderColor = "green";
+    }
+
+
+
+
+
+    console.log(newUser);
+
+
+        
+        
     
 }
 

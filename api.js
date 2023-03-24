810bc9eb428f7a5f13b19a060525c696bbf681d3
DylanEngle <104050134+DylanEngle@users.noreply.github.com>
2023-03-23 23:38:26 -0400

    Register works now, changed title + favicon

--- api.js
+++ api.js
@@ -75,6 +75,15 @@ exports.setApp = function ( app, client )
       error = e.toString();
     }
     var ret = { error: error };
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    );
+    res.setHeader(
+      'Access-Control-Allow-Methods',
+      'GET, POST, PATCH, DELETE, OPTIONS'
+    );
     res.status(200).json(ret);
   });
 
--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -10,58 +10,108 @@ function Register()
 {
     console.log("In Register");
 
-    const newUser = { /* Gathers User input to Register */
-        RFirstName: document.getElementById("registerNameFirst").value,  
-        RLastName: document.getElementById("registerNameLast").value,
-        REmail: document.getElementById("registerEmail").value,
-        RUsername: document.getElementById("registerUsername").value,
-        RPassword: document.getElementById("registerPassword").value,
-    }
+        console.log("made it in doRegister");
 
-    if(newUser.RFirstName == ''){
-        document.getElementById('registerNameFirst').placeholder = "Please enter a first name!";
-        document.getElementById('registerNameFirst').style.borderColor = "red";
-    }else {
-        document.getElementById('registerNameFirst').style.borderColor = "green";
-    }
+        var bp = require('./Path.js');
+        var storage = require('../tokenStorage.js');
 
-    if(newUser.RLastName == ''){
-        document.getElementById("registerNameLast").placeholder='Please enter a last name!';
-        document.getElementById('registerNameLast').style.borderColor = "red";
-    }else {
-        document.getElementById('registerNameLast').style.borderColor = "green";
-    }
+
+        const newUser = { /* Gathers User input to Register */
+        firstName: document.getElementById("registerNameFirst").value,  
+        lastName: document.getElementById("registerNameLast").value,
+        email: document.getElementById("registerEmail").value,
+        login: document.getElementById("registerUsername").value,
+        password: document.getElementById("registerPassword").value,
+        }
+
+        let isEmptyField = false;
+        if(newUser.firstName == ''){
+            document.getElementById('registerNameFirst').placeholder = "Please enter a first name!";
+            document.getElementById('registerNameFirst').style.borderColor = "red";
+            isEmptyField = true;
+        }else {
+            document.getElementById('registerNameFirst').style.borderColor = "green";
+        }
     
-    if(newUser.REmail == ''){
-        document.getElementById("registerEmail").placeholder='Please enter an email!';
-        document.getElementById('registerEmail').style.borderColor = "red";
-    }else {
-        document.getElementById('registerEmail').style.borderColor = "green";
-    }
-
-    if(newUser.RUsername == ''){
-        document.getElementById("registerUsername").placeholder='Please enter a username!';
-        document.getElementById('registerUsername').style.borderColor = "red";
-    }else {
-        document.getElementById('registerUsername').style.borderColor = "green";
-    }
-
-    if(newUser.RPassword == ''){
-        document.getElementById("registerPassword").placeholder='Please enter a password!';
-        document.getElementById('registerPassword').style.borderColor = "red";
-    }else {
-        document.getElementById('registerPassword').style.borderColor = "green";
-    }
-
-
-
-
-
-    console.log(newUser);
-
-
+        if(newUser.lastName == ''){
+            document.getElementById("registerNameLast").placeholder='Please enter a last name!';
+            document.getElementById('registerNameLast').style.borderColor = "red";
+            isEmptyField = true;
+        }else {
+            document.getElementById('registerNameLast').style.borderColor = "green";
+        }
         
-        
+        if(newUser.email == ''){
+            document.getElementById("registerEmail").placeholder='Please enter an email!';
+            document.getElementById('registerEmail').style.borderColor = "red";
+            isEmptyField = true;
+        }else {
+            document.getElementById('registerEmail').style.borderColor = "green";
+        }
+    
+        if(newUser.login == ''){
+            document.getElementById("registerUsername").placeholder='Please enter a username!';
+            document.getElementById('registerUsername').style.borderColor = "red";
+            isEmptyField = true;
+        }else {
+            document.getElementById('registerUsername').style.borderColor = "green";
+        }
+    
+        if(newUser.password == ''){
+            document.getElementById("registerPassword").placeholder='Please enter a password!';
+            document.getElementById('registerPassword').style.borderColor = "red";
+            isEmptyField = true;
+        }else {
+            document.getElementById('registerPassword').style.borderColor = "green";
+        }
+
+        if(isEmptyField){
+            return;
+        }
+        let js = JSON.stringify(newUser);
+        console.log("This is JSON: " + js);
+
+        let config =
+        {
+            method: 'post',
+            url: bp.buildPath('api/register'),
+            headers:
+            {
+            'Content-Type': 'application/json'
+            },
+            data: js
+        };
+        console.log(config);
+        axios(config)
+        .then(function (response)
+        {
+            var res = response.data;
+            console.log("Response is: " , response);
+    
+            if (res.error != 'success')
+            {
+                //setMessage('Failed to register. Please try again.');
+                console.log("Failed to register.")
+            }
+            else
+            {
+                storage.storeToken(res);
+                /*var jwt = require('jsonwebtoken');
+                var ud = jwt.decode(storage.retrieveToken(),{complete:true});
+                var userId = ud.payload.userId;
+                var firstName = ud.payload.firstName;
+                var lastName = ud.payload.lastName;
+                var user = {firstName:firstName,lastName:lastName,id:userId}
+                localStorage.setItem('user_data', JSON.stringify(user));
+                */
+                console.log("User successfully registered.");
+            }
+        })
+
+        .catch(function (error)
+        {
+            console.log(error);
+        });
     
 }
 

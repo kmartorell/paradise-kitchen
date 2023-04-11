287ae6ee475c5af47e58d413b2584d55b59d0490
EvanVerdu <89754847+EvanVerdu@users.noreply.github.com>
2023-04-11 11:28:25 -0400

    Create ViewBody.js

---
+++ frontend/src/components/ViewBody.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../css/View.css'
+
+function PageTitle()
+{
+   return(
+        <center className='HomePageBox'>
+                <h1 id="HomePageWords">
+                    View your Recipes Page!
+                </h1>
+
+        </center>
+   );
+};
+export default PageTitle;

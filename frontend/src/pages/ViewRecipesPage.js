1897a8fb3cc5310560cc2d6ae7873d47279dcede
EvanVerdu <89754847+EvanVerdu@users.noreply.github.com>
2023-04-11 11:22:15 -0400

    Create ViewRecipesPage.js

---
+++ frontend/src/pages/ViewRecipesPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ViewNavBar from '../components/ViewNavBar.js';
+import ViewBody from '../components/ViewBody.js';
+
+const landingPage = () =>
+{
+    return(
+        <div>
+            <ViewNavBar />
+            <ViewBody />
+        </div>
+    );
+}
+export default landingPage;

1ceff9c1b6e2ec7e0cdb6e695b879fe57488dba3
Brandon Blake <brandonblake997@gmail.com>
2023-04-19 19:19:21 -0400

    YourRecipes testing

--- app/components/YourRecipes.js
+++ app/components/YourRecipes.js
@@ -1,77 +1,333 @@
 import React, { useState, useEffect, setState } from 'react';
-import { StyleSheet, SafeAreaView, TextInput, Text, View, Button, Alert, Image, ImageBackground, TouchableOpacity } from 'react-native';
+import { StyleSheet, SafeAreaView, TextInput, Text, View, Svg, Path, Button, Alert, Card, Image, ImageBackground, TouchableOpacity, ScrollView } from 'react-native';
 import Images from './Images';
 import axios from 'axios';
 
-const YourRecipes = ({navigation, route}) =>
+const SearchRecipes = ({navigation, route}) =>
 { 
+    const [search, onChangeSearch] = React.useState('');
+    const [results, setResults] = React.useState('');
+    const [user, setUser] = React.useState('');
+    
 
-  return(
-    <ImageBackground source={Images.background} resizeMode="cover" style={styles.image}>
-        <SafeAreaView style={styles.container}>
-            <Image source={Images.logo} style={styles.logo} />
-            <View style={styles.mainLanding}>
-                <View style={styles.buttonHolder}>
-                  <Text style={styles.header}>View your Recipes Page!</Text>
+    const doSearch = async (search) =>
+    {
+      if(search == '')
+        onChangeSearch('');
+      fetch('https://paradise-kitchen.herokuapp.com/api/searchrecipe', {
+          method: 'POST',
+          headers: {
+              'Accept': 'application/json',
+              'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+              text: search,
+          })
+        })    
+        .then(response => response.json())
+        .then(json => {
+            setResults(json);
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    };
+
+    const renderCard = (card, index) => {
+      return(
+        <TouchableOpacity style={styles.cardMain} key={card.id} onPress={() => navigation.navigate('ViewRecipe', {recipe: card, user:user})}>
+              <View style={styles.cardInfo}>
+                <Text style={styles.cardTitle}>
+                  {card.name.toUpperCase()}
+                </Text>
+                <Text style={styles.cardTags}>
+                  {card.tags.map(i => '+' + i).slice(0,4).join('\n')}
+                </Text>
+                <Text style={styles.cardDescription}>
+                  {card.shortDescription}
+                </Text>
+              </View>
+              <View style={styles.cardButton}>
+                <View>
+                  <Image source={Images.recipeClickIcon} style={styles.buttonIcon} />
+                </View>
+              </View>
+        </TouchableOpacity>
+      );
+    };
+    
+    useEffect(() => {
+      const doSearch = navigation.addListener('focus',() =>
+      {
+        fetch('https://paradise-kitchen.herokuapp.com/api/searchrecipe', {
+          method: 'POST',
+          headers: {
+              'Accept': 'application/json',
+              'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+              text: '',
+          })
+        })    
+        .then(response => response.json())
+        .then(json => {
+            setResults(json);
+        })
+        .catch(error => {
+          console.error(error);
+        });
+        // Grab user info
+        fetch('https://paradise-kitchen.herokuapp.com/api/getUser', {
+          method: 'POST',
+          headers: {
+              'Accept': 'application/json',
+              'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+              userId: route.params.user.id,
+          })
+        })    
+        .then(response => response.json())
+        .then(json => {
+            setUser(json);
+        })
+        .catch(error => {
+          console.error(error);
+        });
+      });
+      return doSearch;
+    }, [navigation]);
+
+    return(
+      <ImageBackground source={Images.background} resizeMode="cover" style={styles.image}>
+          <ScrollView style={styles.scrollView} contentInsetAdjustmentBehavior="automatic">
+            <SafeAreaView style={styles.container}>
+                <Image source={Images.logo} style={styles.logo} />
+                <Text style={styles.header}>Paradise Kitchen</Text>
+                <View style={styles.mainLanding}>
+                  <Text style={styles.subheader}>Search Recipes Here!</Text>
+                    <TextInput
+                        style={styles.input}
+                        onChangeText={onChangeSearch}
+                        value={search}
+                        placeholder="Type in a Name, Description, Ingredient or Tag here."
+                    />
+                    <TouchableOpacity style={styles.buttonStyle} onPress={() => doSearch(search)}>
+                        <Text style={styles.buttonText}>Search</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.buttonStyle} onPress={() => doSearch('')}>
+                        <Text style={styles.buttonText}>Reset</Text>
+                    </TouchableOpacity>
                     <TouchableOpacity style={styles.buttonStyle} onPress={() => navigation.navigate('Landing', {firstName: route.params.firstName})}>
                         <Text style={styles.buttonText}>Home</Text>
                     </TouchableOpacity>
                 </View>
-            </View>
-        </SafeAreaView>
-  </ImageBackground>
-);
+                { results && 
+                <View style={styles.recipeList}>
+                  <Text style={styles.subheader}>Recipe List</Text>
+                    { results.error != "search fail" && 
+                      results.map(renderCard)
+                    }
+                    { results.error == "search fail" && 
+                    <View style={styles.noResultsDiv} >
+                      <Text style={styles.noResults}>No Results Found</Text>
+                    </View>
+                    }
+                </View>
+                }    
+            </SafeAreaView>
+          </ScrollView>
+      </ImageBackground>
+  );
 };
-
-
 const styles = StyleSheet.create({
-  logo: {
-      height: 125,
-      width: 125,
-      margin: 10,
-  },
-  header: {
-      fontSize: 40,
-      textAlign: 'center',
-  },
-  container: {
-    alignItems: 'center',
-    height:'100%'
-  },
-  mainLanding: {
-    width:'90%',
-    backgroundColor:'white',
-    alignItems:'center',
-    padding:22,
-    borderRadius:20,
-  },
-  message: {
-      fontSize: 20,
-      color: 'green',
-      textAlign: 'center',
+  input: {
+      height: 50,
+      width:"90%",
+      margin: 12,
+      borderWidth: 1,
+      padding: 10,
+      borderRadius:10,
   },
   buttonStyle:{
     backgroundColor:'orange',
     width:"80%",
     padding:8,
-    marginTop:20,
-    marginBottom:12,
+    marginTop:12,
+    marginBottom:6,
     borderRadius:20,
   },
-  searchRecipeText: {
-    color: 'white',
-    fontSize: 30,
-    textAlign: 'center',
+  textInputTitle: {
+    color: 'black',
+    fontSize: 27,
+    fontWeight: 'bold',
+  },
+  textInputStyle: {
+    color: 'black',
+    fontSize: 15,
+    borderWidth: 1,
+    height: 50,
+    width: '100%',
+    marginBottom: 30,
+    borderRadius: 10,
+    paddingLeft: 10,
+  },
+  textBottomInputStyle: {
+    color: 'black',
+    fontSize: 15,
+    borderWidth: 1,
+    height: 50,
+    width: '100%',
+    marginBottom: 20,
+    borderRadius: 10,
+    paddingLeft: 10,
   },
   buttonText: {
     color: 'white',
-    fontSize: 35,
+    fontSize: 20,
     textAlign: 'center',
   },
-  buttonHolder: {
-    alignItems: 'center',
-    marginTop: 10,
-    width : '100%',
+  container: {
+      alignItems: 'center',
+      height:'100%'
+  },
+  mainLanding: {
+      width:'90%',
+      backgroundColor:'white',
+      alignItems:'center',
+      padding:22,
+      borderRadius:20,
+      marginTop:30,
+  },
+  recipeList: {
+    width:'90%',
+    backgroundColor:'white',
+    padding:22,
+    borderRadius:20,
+    marginTop:30,
 },
+  logo: {
+      width:125,
+      height:125
+  },
+  header: {
+      fontSize: 40,
+      textAlign: 'center',
+      fontWeight:'bold',
+  },
+  formButtons:{
+      marginTop:10,
+      flexDirection:'row',
+      alignItems:'center',
+      justifyContent:'center',
+      shadowColor:'black',
+      shadowOpacity:0.25,
+      shadowRadius:10,
+      width:'60%',
+  },
+  subheader: {
+      fontSize: 20,
+      textAlign: 'center',
+      fontWeight:'bold',
+      width:"100%",
+      marginTop:20,
+  },
+  message: {
+      fontSize: 20,
+      color: 'green',
+      textAlign: 'center',
+      marginTop:16,
+  },
+  errorMessage: {
+      fontSize: 20,
+      color: 'red',
+      textAlign: 'center',
+  },
+  loginBox:{
+      backgroundColor:'orange',
+      width:'70%',
+      borderRadius:20,
+      marginRight:-16,
+      zIndex:99,
+  },
+  searchRecipeText: {
+    color: 'black',
+    fontSize: 15,
+    borderWidth: 1,
+    height: 50,
+    borderRadius: 10,
+  },
+  registerBox:{
+      width:'70%',
+      backgroundColor:'white',
+      borderTopRightRadius:20,
+      borderBottomRightRadius:20,
+  },
+  loginButton:{
+      color:"#ffffff",
+  },
+  submitButton:{
+      backgroundColor:'orange',
+      width:"80%",
+      padding:8,
+      marginTop:20,
+      marginBottom:12,
+      borderRadius:20,
+  },
+  scrollView: {
+    height:'100%'
+  },
+  cardMain: {
+    flex:1,
+    flexDirection:'row',
+    height:160,
+    overflow:'hidden',
+    margin:6,
+    textAlign:'left',
+    borderLeftColor:'orange',
+    borderLeftWidth:5,
+    padding:12,
+  },  
+  cardTitle: {
+    textAlign:'left',
+    fontWeight:'bold',
+    fontSize:16,
+
+  },
+  cardDescription: {
+    textAlign:'left',
+  },
+  cardInfo: {
+    flex:15,
+    width:'90%'
+  },  
+  cardButton: {
+    flex:1,
+    justifyContent:'center',
+    paddingLeft:6
+  },  
+  cardTags: {
+    color:'grey',
+    width:'100%',
+    paddingTop:4,
+    paddingBottom:4
+  },  
+  buttonIcon: {
+    marginTop:'auto',
+    width:30,
+    height:30
+  },
+  noResults: {
+    textAlign:'center',
+    justifyContent:'center',
+    fontSize:16,
+  },  
+  noResultsDiv: {
+    paddingTop:20,
+    textAlign:'center',
+    justifyContent:'center',
+    width:'100%'
+  }  
+
 });
-export default YourRecipes;
+export default SearchRecipes;

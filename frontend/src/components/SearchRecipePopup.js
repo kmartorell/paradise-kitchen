bbdcca3d6e2c2db6ced9556811f2e639ec423c6c
EvanVerdu <89754847+EvanVerdu@users.noreply.github.com>
2023-04-20 15:06:59 -0400

    Update SearchRecipePopup.js

--- frontend/src/components/SearchRecipePopup.js
+++ frontend/src/components/SearchRecipePopup.js
@@ -2,12 +2,17 @@ import React from "react";
 import '../css/Search.css';
  
 const Popup = props => {
+
+  function addedtofav() {
+    document.getElementById("PopupButton").innerHTML = "Added!";
+  }
+
   return (
     <div className="popup-box">
       <div className="popbox">
          {props.content}
         <button id="PopupEditButton" onClick={props.EditRecipe1}>Edit Recipe</button>
-        <button id="PopupButton" onClick={props.AddRecipe}>Favorite Recipe</button>
+        <button id="PopupButton" onClick={() => {props.AddRecipe(); addedtofav()}}>Favorite Recipe</button>
         <button id="PopupCloseButton" onClick={props.handleClose}>Return</button>
         <button id="PopupDeleteButton" onClick={props.DeleteRecipe1}>Delete Recipe</button>
       </div>
